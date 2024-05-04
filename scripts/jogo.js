27071055aa0b3dbb05a66be6259350f75f7023fe
Guilherme Monni <gmonni20@gmail.com>
2024-05-04 00:38:03 -0300

    ajustando botões do jogo da velha

--- scripts/jogo.js
+++ scripts/jogo.js
@@ -1,15 +1,23 @@
 function btnVoltar(){
-    location.replace("index.html", "_self")
-}
-
-function btnJogarJokenpo(){
-    location.replace("jokenpo.html", "_self")
-}
-
-function btnRegrasJokenpo(){
-    location.replace("regras-jokenpo.html", "_self")
-}
+    location.replace("index.html", "_self");
+};
 
 function btnRedes(){
-    location.replace("redes.html", "_self")
-}
+    location.replace("redes.html", "_self");
+};
+
+function btnJogarJokenpo(){
+    location.replace("jokenpo.html", "_self");
+};
+
+function btnRegrasJokenpo(){
+    location.replace("regras-jokenpo.html", "_self");
+};
+
+function btnJogarVelha(){
+    location.replace("velha.html", "_self");
+};
+
+function btnRegrasVelha(){
+    location.replace("regras-velha.html", "_self");
+};
