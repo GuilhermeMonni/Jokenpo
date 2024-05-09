d64474a9af8a620b21282f01fb875ed31a467a69
Guilherme Monni <gmonni20@gmail.com>
2024-05-09 11:30:52 -0300

    animação de boa sorte

--- scripts/velha.js
+++ scripts/velha.js
@@ -4,8 +4,10 @@ function btnVoltar(){
 
 function carregaPag(){
     let quadro = document.querySelector(".quadro");
+    let btn_jogar = document.querySelector("#btn-jogar");
 
     quadro.style.display = "none";
+    btn_jogar.style.display = "none";
 };
 
 function escolhaXis(){
