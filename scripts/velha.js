332611a1385ec0b6660fe5b2a5822aac6050ba1f
Guilherme Monni <gmonni20@gmail.com>
2024-05-11 10:33:36 -0300

    ajuste botoes jogo da velha

--- scripts/velha.js
+++ scripts/velha.js
@@ -6,44 +6,57 @@ function btnVoltar(){
 function carregaPag(){
     //função quando carrega a pag
     let quadro = document.querySelector(".quadro");
-    let btn_jogar = document.querySelector("#btn-jogar");
+    let btn_jogar_xis = document.querySelector("#btn-jogar-xis");
+    let btn_jogar_bola = document.querySelector("#btn-jogar-bola");
 
     quadro.style.display = "none";  //tabela do jogo
-    btn_jogar.style.display = "none";   //botao de jogar
+    btn_jogar_xis.style.display = "none";   //botao de jogar xis
+    btn_jogar_bola.style.display = "none";   //botao de jogar bola
 };
 
-function escolhaXis(escolha){
+function escolhaXis(){
     //botao de escolha do xis
     let btn_xis = document.querySelector("#btn-xis");   //botao do xis
     let btn_bola = document.querySelector("#btn-bola");   //botao da bolinha
-    let btn_jogar = document.querySelector("#btn-jogar");   //botao de jogar
+    let btn_jogar_xis = document.querySelector("#btn-jogar-xis");   //botao de jogar xis
     let boa_sorte = document.querySelector("#boa-sorte");   //texto de boa sorte
     let emoji = document.querySelector("#emoji");   //emoji de animação
+    let btn_jogar_bola = document.querySelector("#btn-jogar-bola");   //botao de jogar bolinha
 
     btn_bola.style.background = "#ed1c23";
     btn_bola.style.color = "black";
     emoji.style.display = "none";
     boa_sorte.style.display = "initial";
-    btn_jogar.style.display = "initial";
+    btn_jogar_xis.style.display = "initial";
+    btn_jogar_bola.style.display = "none";
     btn_xis.style.background = "#eb0008da";
     btn_xis.style.color = "#3ADC64";
-    return escolha = "x";
 }
 
-function escolhaBola(escolha){
+function escolhaBola(){
     //botao de escolha do bola
     let btn_bola = document.querySelector("#btn-bola");   //botao da bolinha
     let btn_xis = document.querySelector("#btn-xis");   //botao do xis
-    let btn_jogar = document.querySelector("#btn-jogar");   //botao de jogar
+    let btn_jogar_bola = document.querySelector("#btn-jogar-bola");   //botao de jogar bolinha
     let boa_sorte = document.querySelector("#boa-sorte");   //texto de boa sorte
     let emoji = document.querySelector("#emoji");   //emoji de animação
+    let btn_jogar_xis = document.querySelector("#btn-jogar-xis");   //botao de jogar xis
 
     btn_xis.style.background = "#ed1c23";
     btn_xis.style.color = "black";
     emoji.style.display = "none";
     boa_sorte.style.display = "initial";
-    btn_jogar.style.display = "initial";
+    btn_jogar_bola.style.display = "initial";
+    btn_jogar_xis.style.display = "none";
     btn_bola.style.background = "#eb0008da";
     btn_bola.style.color = "#3ADC64";
-    return escolha = "o";
 }
+
+function jogarXis(){
+    alert("escolheu xis");
+}
+
+function jogarBola(){
+    alert("escolheu bola");
+}
+
