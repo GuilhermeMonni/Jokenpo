a0faed2770eedfd7a82a77fbbded99ae1b9ab653
Guilherme Monni <gmonni20@gmail.com>
2024-06-06 12:40:20 -0300

    jogo da velha x

--- scripts/velha-jogo-x.js
+++ scripts/velha-jogo-x.js
@@ -3,6 +3,8 @@ function btnVoltar(){
     location.replace("index.html", "_self");
 }
 
+window.addEventListener('load', inicia);  //quando inicializar a pagina
+
 const status = document.querySelector(".status");
 // status da partida
 const btn_restart = document.querySelector("btn_restart");
@@ -14,119 +16,13 @@ let jogador = 0;  //0-jogador \ 1-computador
 let jogo = true;    //status do jogo
 let status_jogo = []; //jogadas
 let nivel = 1 //nivel jogada cpu
-let vencedor 
+let vencedor //vencedor da rodada
+let verifica    //verifica vitória
 
-function click_celula(p){
-    //função quando escolher uma celula
-    if((jogo == true) && (jogador == 0)){
-        switch(p){
-            case 1:
-                if(status_jogo[0][0] == ""){
-                    status_jogo[0][0] = "X";
-                    jogador = 1;
-                }
-                break;
-            case 2:
-                if(status_jogo[0][1] == ""){
-                    status_jogo[0][1] = "X";
-                }
-                break;
-            case 3:
-                if(status_jogo[0][2] == ""){
-                    status_jogo[0][2] = "X";
-                }
-                break;
-            case 4:
-                if(status_jogo[1][0] == ""){
-                    status_jogo[1][0] = "X";
-                }
-                break;
-            case 5:
-                if(status_jogo[1][1] == ""){
-                    status_jogo[1][1] = "X";
-                }
-                break;
-            case 6:
-                if(status_jogo[1][2] == ""){
-                    status_jogo[1][2] = "X";
-                }
-                break;
-            case 7:
-                if(status_jogo[2][0] == ""){
-                    status_jogo[2][0] = "X";
-                }
-                break;
-            case 8:
-                if(status_jogo[2][1] == ""){
-                    status_jogo[2][1] = "X";
-                }
-                break;
-            case 9:
-                if(status_jogo[2][2] == ""){
-                    status_jogo[2][2] = "X";
-                }
-                break;
-        }
-    }
-    
-    jogada_computador(); //chama a jogada da cpu
-    atualiza_tabuleiro(); //atualiza o tabuleiro
-    checar_resultado(); // checa se não tem um vencedor
-}
-
-function jogada_computador(){
-    //jogada da cpu
-    if((jogo == true) && (jogador = 1)){
-        let c = 0;  //coluna
-        let l = 0;  //linha
-        if(nivel == 1){
-            do{ //manda a cpu sortear um número
-                c = Math.round(Math.random()*2);
-                l = Math.round(Math.random()*2);
-            }while(status_jogo[l][c] != "");
-            status_jogo[l][c] = "O";
-        }
-        atualiza_tabuleiro();
-        checar_resultado();
-        jogador = 0;
-    }
-}
-
-function checar_resultado(){    
-    if((status_jogo[0][0] === "X") && (status_jogo[0][1] === "X") && (status_jogo[0][2] === "X")){
-        atualiza_tabuleiro();
-        vencedor = 0;
-        const resultado = () => {alert("Xis venceu")};
-        setTimeout(resultado, 1000);    //temporizador
-    }else if((status_jogo[0][0] === "O") && (status_jogo[0][1] === "O") && (status_jogo[0][2] === "O")){
-        atualiza_tabuleiro();
-        vencedor = 1;
-        const resultado = () => {alert("Bolinha venceu")};
-        setTimeout(resultado, 1000);    //temporizador
-    }
-}
-
-function atualiza_tabuleiro(){
-    //posiçoes no tabuleiro
-    for(let l = 0; l < 3; l++){
-        for(let c = 0; c < 3; c++){
-            if(status_jogo[l][c] === "X"){
-                tabuleiro[l][c].textContent = "X";
-                tabuleiro[l][c].style.cursor = "default";
-            }else if(status_jogo[l][c] === "O"){
-                tabuleiro[l][c].textContent = "O";
-                tabuleiro[l][c].style.cursor = "default";
-            }else{
-                tabuleiro[l][c].textContent = "";
-                tabuleiro[l][c].style.cursor = "pointer";
-            }
-        }
-    }
-}
-
-function inicia(){
-    jogo = true;
-    status_jogo = [
+function inicia(){  //quando carrega a pag
+    jogador = 0;
+    jogo = true;    //inicia jogo
+    status_jogo = [ //zera o tabuleiro
         ["", "", ""], 
         ["", "", ""], 
         ["", "", ""]
@@ -139,9 +35,128 @@ function inicia(){
     ]
 }
 
-window.addEventListener('load', inicia);  //quando inicializar a pagina
+function checar_resultado(){    
+    // checa se não tem um vencedor
+    for(l = 0; l < 3; l++){ //verifica linhas
+        if((status_jogo[l][0] == status_jogo[l][1]) && (status_jogo[l][1] == status_jogo[l][2])){
+            return status_jogo[l][0];
+        }
+    }
+    for(c = 0; c < 3; c++){//verifica colunas
+        if((status_jogo[0][c] == status_jogo[1][c]) && (status_jogo[1][c] == status_jogo[2][c])){
+            return status_jogo[0][c];
+        }
+    }
+    if((status_jogo[0][0] == status_jogo[1][1]) && (status_jogo[1][1] == status_jogo[2][2])){//verifica diagonal
+        return status_jogo[0][0];
+    }else if((status_jogo[0][2] == status_jogo[1][1]) && (status_jogo[2][0])){//verifica outra diagonal
+        return status_jogo[0][2];
+    }
+    return "";
+}
 
+function atualiza_tabuleiro(){
+    //posiçoes no tabuleiro
+    for(let l = 0; l < 3; l++){ //percorre as linhas da matriz
+        for(let c = 0; c < 3; c++){//percorre as colunas 
+            if(status_jogo[l][c] === "X"){  //se o xis selecionar
+                tabuleiro[l][c].textContent = "X";
+                tabuleiro[l][c].style.cursor = "default";
+            }else if(status_jogo[l][c] === "O"){ //se O selecionar
+                tabuleiro[l][c].textContent = "O";
+                tabuleiro[l][c].style.cursor = "default";
+            }else{
+                tabuleiro[l][c].textContent = "";
+                tabuleiro[l][c].style.cursor = "pointer";
+            }
+        }
+    }
+}
 
+function pos_selecionadas(){
+    for(l = 0; l < 3; l++){
+        for(c = 0; c < 3; c++){
+            if(status_jogo[l][c] == ""){
+                return jogo = true;
+            }else if(status_jogo[l][c] != ""){
+                return jogo = false;
+            }
+        }
+    } 
+}
 
+function click_celula(p){
+    //função quando escolher uma celula
+    if((jogo) && (jogador == 0)){
+        pos_selecionadas();
+        switch(p){
+            case 1:
+                status_jogo[0][0] = "X";
+                break;
+            case 2:
+                status_jogo[0][1] = "X";
+                break;
+            case 3:
+                status_jogo[0][2] = "X";
+                break;
+            case 4:
+                status_jogo[1][0] = "X";
+                break;
+            case 5:
+                status_jogo[1][1] = "X";
+                break;
+            case 6:
+                status_jogo[1][2] = "X";
+                break;
+            case 7:
+                status_jogo[2][0] = "X";
+                break;
+            case 8:
+                status_jogo[2][1] = "X";
+                break;
+            case 9:
+                status_jogo[2][2] = "X";
+                break;
+        }
+    }
 
+    atualiza_tabuleiro(); //atualiza o tabuleiro
+    verifica = checar_resultado(); //checa se tem um vencedor
+    if(verifica == "X"){//verifica e avisa o vencedor
+        alert("Parabéns você venceu!");
+        jogo = false;
+        inicia();
+    }else if(verifica == "O"){
+        alert("HAHA! Você perdeu");
+        jogo = false;
+        inicia();
+    }
+    jogada_computador(); //chama a jogada da cpu
+}
 
+function jogada_computador(){
+    //jogada da cpu
+    if((jogo) && (jogador = 1)){
+        let c = 0;  //coluna
+        let l = 0;  //linha
+        if(nivel == 1){
+            do{ //manda a cpu sortear um número
+                c = Math.round(Math.random()*2);
+                l = Math.round(Math.random()*2);
+            }while(status_jogo[l][c] != "");
+            status_jogo[l][c] = "O";
+        }
+        atualiza_tabuleiro();
+        verifica = checar_resultado(); //checa se tem um vencedor
+        if(verifica == "X"){//verifica e avisa o vencedor
+            alert("Parabéns você venceu!");
+            jogo = false;
+            inicia();
+        }else if(verifica == "O"){
+            alert("HAHA! Você perdeu");
+            jogo = false;
+            inicia();
+        }
+    }
+    jogador = 0;//chama a jogada do jogador
+}
